f6df9b17e67e50586e0e45f5b54f157114bc89b1
SatanistiVuohi <kumpulatommi@gmail.com>
2024-10-14 17:47:26 +0300

    v1.04 -Added styles for the header and image

--- src/ComponentUI.jsx
+++ src/ComponentUI.jsx
@@ -4,10 +4,22 @@ import logo from './assets/logo.png'; // You can adjust the path if necessary.
 
 export function Header() {
   return (
-    <header>
-      <img src={logo} alt="logo" />
+    <header style={headerStyle.header}>
+      <img src={logo} alt="logo" style={headerStyle.image} />
       <h1>Welcome to product page!</h1>
     </header>
   );
 }
+
+const headerStyle = {
+    header: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    image: {
+        width: '75px',
+        height: '75px'
+    }
+}
+
 export default Header;
