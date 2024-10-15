43afdaf0ffec291e6fd1c5c717a1a3b942f84f7e
SatanistiVuohi <kumpulatommi@gmail.com>
2024-10-15 17:59:19 +0300

    v1.00 -Added the file for the API, basic stuff

---
+++ OpenData.jsx
@@ -0,0 +1,12 @@
+
+
+function Trivia() {
+
+    return (
+        <div>
+        <h2>Get a random number and a fact about it!</h2>
+        <button>Click me!</button>
+        <h3>Results</h3>
+        </div>
+    )
+};
